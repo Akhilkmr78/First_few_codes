5eb90a350a29f45c6fe20702cbafb57c7c3b4c0d
Akhilkmr78 <16m602@nith.ac.in>
2020-06-18 13:37:48 +0530

    Changed Subject for test

--- mailers.py
+++ mailers.py
@@ -4,7 +4,7 @@ from email.message import EmailMessage
 email = EmailMessage()
 email['from'] = 'Akhil Kumar'
 email['to'] = '16m602@nith.ac.in'
-email['subject'] = 'Sent from Pycharm!'
+email['subject'] = 'Sent from Pycharm!!'
 
 email.set_content('Learning to grow')
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
